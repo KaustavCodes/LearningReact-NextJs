fe0c23c656297373ba65d2808801012d76dff880
Kaustav Halder <96991352+KaustavCodes@users.noreply.github.com>
2024-10-02 13:09:55 +0000

    Save To Git

---
+++ LearningReact/src/App.jsx
@@ -0,0 +1,14 @@
+import { Post } from './components/Post';
+
+function App() {
+  return (
+    <>
+      <h1>Hello World!</h1>
+
+      <Post></Post>
+    </>
+  );
+  
+}
+
+export default App;
---
+++ LearningReact/src/components/Post.jsx
@@ -0,0 +1,8 @@
+export function Post() {
+    let name = 'Kaustav';
+    return(
+        <div>
+            <h1>A demo name is {name}</h1>
+        </div>
+    )
+}
